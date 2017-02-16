e8073ffa81fc361aea01aef4e8de37ed2eb6e80a
Jon Carstens <jon.carstens@mx.com>
2017-02-16 15:27:36 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Ofex.Mixfile do
       app: :ofex,
       name: "Ofex",
       description: "A simple parser for Open Financial Exchange (OFX) data in elixir",
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.4",
       package: [
         maintainers: ["Jon Carstens"],
